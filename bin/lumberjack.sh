38f252e251d476907beef0556891e7aa148994e6
Rich Williams <M>
2019-08-01 14:42:01 -0700

    still working on the lumberjack, but added in the core $HOME files

--- bin/lumberjack.sh
+++ bin/lumberjack.sh
@@ -1,44 +1,163 @@
 #!/usr/bin/env bash
 
-## Prepends a timestamp and a few carriage returns to /home/richw/desktop/pastebin
-## and then opens the file in gvim for editing.
-##
-## NOTES on expected symlink structure:
-##      $HOME/desktop/pastebin  ->  $HOME/personal/pastebin
-##      $HOME/personal          ->  /data/personal/$USER
-##      /data/personal          ->  /data/$HOST/personal
-##
-## Borrowed and adapted from https://stackoverflow.com/a/3272296
+#####################################################################
+### \brief      Prepends a timestamp and a few carriage returns to 
+###             the file specified as a parameter, and then opens 
+###             the file in gvim for editing.
+### \since      19 Jul 2019     
+### \details    Adapted from https://stackoverflow.com/a/3272296
+
+#####################################################################
+# CONSTANTS
+### https://unix.stackexchange.com/a/198926
+#readonly THIS_FILE=`basename -- "$0"`;
+### https://unix.stackexchange.com/a/198931
+readonly THIS_FILE=`printf '%s\n' "${0##*/}"`;
 
 readonly PASTEBIN_FILE="$HOME/personal/pastebin";
 readonly SYMLINK_TO_PASTEBIN_FILE="$HOME/desktop/pastebin";
 
+### Follow the C/C++ conventions
+readonly FALSE=0;
+### This should be called as,  `[[ ! "${FALSE}" ]]` 
+### ...b/c in C a "non-zero value" == TRUE
+readonly TRUE=1;
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Exit Codes
+readonly EXIT_SUCCESS=0;
+readonly EXIT_BAD_ARG=1;
+readonly EXIT_MISSING_REQUIREMENT=2;
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Script variables
+timestamp_args='-z';
+interactive_mode="${TRUE}";
+exit_msg="";
+
+
+
+#####################################################################
+# FUNCTIONS
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+### Sets the exit code and/or error message
+### \param  <int>
+function    exit_script()   {
+    if [[ "${EXIT_SUCCESS}" == "${1}" ]]; then
+        # First: if all is good, bail out.
+        exit "${EXIT_SUCCESS}";
+    fi
+    
+    # Well, something went wrong :-( 
+    if [[ "${interactive_mode}" != "${FALSE}" ]]; then
+        if [[  ]]
+        fi
+    else
+
+    fi
+}
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+### Makes sure that script requirements are met
+function    sanity_check()  {
+# Make sure shit exists
+    if [[ ! -f ${PASTEBIN_FILE} ]]; then
+        touch ${PASTEBIN_FILE};
+    fi
+
+    if [[ ! -L ${SYMLINK_TO_PASTEBIN_FILE} ]]; then
+        ln -s "${PASTEBIN_FILE}"  "${SYMLINK_TO_PASTEBIN_FILE}";
+    fi
+
+    ## Good GAWD. `ed` wasn't installed by default on SLE_15
+    which ed > /dev/null 2>&1;
+    if [[ 0 -ne "$?" ]]; then
+        exit_msg="Believe it or not, 'ed' is not installed.\n\n`cnf ed`\n\n";
+        exit_script "${EXIT_MISSING_REQUIREMENT}" "${exit_msg}";
+    fi
+}
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+### The menu that appears when needed
+function    lumberjack_help()   {
+    cat << EOF
+
+  USAGE:
+    ${THIS_FILE} [-hgnuy] [-f /path/to/logfile] [/path/to/logfile]
+
+  Option    Description
+  --------- ---------------------------------------------
+  -f 'file' Explicit call to '/path/to/logfile'
+              If 'logfile' does not exist, you will be 
+              prompted if you would like to create it
+              (use "-y" to disable nag prompts).
+    -h      Display (this) help message.
+    -g      Timestamp (whatever format) in UTC (GMT). 
+              "-UTC-" will always appended unless UNIX
+              epoch is used.
+    -n      Append nanoseconds to timestamp.
+    -u      Suffix timestamp with UNIX epoch; e.g.:
+                2019-07-23_13-02-24_-0700_1563912144
+    -y      Non-interactive mode; answer 'yes' to prompts.
+
+EOF
+}
+
+
+
+
+#####################################################################
+# EXECUTION
+sanity_check;
+
+while getopts   "f:hgUy" arg;    do
+    case    "$arg" in
+        f)  ;;
+        h)  lumberjack_help;
+            exit_script 0;
+            ;;
+        g)  timestamp_args="${timestamp_args}g"
+            ;;
+        n)  timestamp_args="${timestamp_args}n"
+            ;;
+        u)  timestamp_args="${timestamp_args}u"
+            ;;
+        y)  interactive_mode="${TRUE}";
+            ;;
+        *)  lumberjack_help;
+            exit_msg="  ERROR: '${OPTARG}' is invalid :-(";
+            exit_script "${EXIT_BAD_ARG}" "${exit_msg}";
+            ;;
+    esac
+done
 
 ## Make sure shit exists
-if [[ ! -f ${PASTEBIN_FILE} ]]; then
-    touch ${PASTEBIN_FILE};
-fi
-
-if [[ ! -L ${SYMLINK_TO_PASTEBIN_FILE} ]]; then
-    ln -s "${PASTEBIN_FILE}"  "${SYMLINK_TO_PASTEBIN_FILE}";
-fi
-
-## Good GAWD. `ed` wasn't installed by default on SLE_15
-which ed > /dev/null 2>&1;
-if [[ 0 -ne "$?" ]]; then
-    echo -e "\n\tBelieve it or not, 'ed' is not installed."
-    echo -e "\tInstall 'ed' and try again "'¯\_(ツ)_/¯'"\n"
-    exit 1;
-fi
-
-echo -e "0a
-# `timestamp`\n\n\n
-.
-w" | ed "${PASTEBIN_FILE}" ;
-
+#if [[ ! -f ${PASTEBIN_FILE} ]]; then
+#    touch ${PASTEBIN_FILE};
+#fi
+#
+#if [[ ! -L ${SYMLINK_TO_PASTEBIN_FILE} ]]; then
+#    ln -s "${PASTEBIN_FILE}"  "${SYMLINK_TO_PASTEBIN_FILE}";
+#fi
+#
+### Good GAWD. `ed` wasn't installed by default on SLE_15
+#which ed > /dev/null 2>&1;
+#if [[ 0 -ne "$?" ]]; then
+#    echo -e "\n\tBelieve it or not, 'ed' is not installed."
+#    echo -e "\tInstall 'ed' and try again "'¯\_(ツ)_/¯'"\n"
+#    exit 1;
+#fi
+#
+#echo -e "0a
+## `timestamp`\n\n\n
+#.
+#w" | ed "${PASTEBIN_FILE}" ;
+#
 ### vim notes:
 ###     -p      Open vim files in a tabbed interface.
 ###     +n      Open file and place cursor at line n
-
-/usr/bin/gvim -p +2 "${PASTEBIN_FILE}" &
+#
+#/usr/bin/gvim -p +2 "${PASTEBIN_FILE}" &
 
